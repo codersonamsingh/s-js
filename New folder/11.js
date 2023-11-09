3e3e90177c37f10a830534e9ebb528a65657b5ae
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-11-09 18:20:31 +0530

    votable running

---
+++ New folder/11.js
@@ -0,0 +1,9 @@
+let age = 19
+let isVotable
+// let isVotable = (age >= 18)  ? "Yes Votable" : "Not Votable"
+if(age >=18) {
+    isVotable = "Yes Votable"
+}else{
+    isVotable ="Not votable"
+}
+console.log(isVotable)
