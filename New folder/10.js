400ab61d89bd398eecf40296d3dd6c04750b8742
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-11-09 17:12:10 +0530

    consol log runn

--- New folder/10.js
+++ New folder/10.js
@@ -1,13 +1,13 @@
 
-if ('a' == 'a'){
-    console.log("if block is running")
-}else{
-    console.log("else block is running")
-}
-let emailLogin = true
-let googleLogin = false
-if(emailLogin || googleLogin){
-    console.log("Show Logout")
-}else{
-    console.log("Show Login")
-}
+//if ('a' == 'a'){
+//    console.log("if block is running")
+//}else{
+ //   console.log("else block is running")
+//}
+//let emailLogin = true
+//let googleLogin = false
+//if(emailLogin || googleLogin){
+   // console.log("Show Logout")
+//}else{
+ //   console.log("Show Login")
+//}
