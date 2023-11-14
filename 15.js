698be993c41e5b753cbe8b7defcfc237dc087142
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-11-14 16:46:09 +0530

    15 js running

--- 15.js
+++ 15.js
@@ -1,7 +1,7 @@
-let add = function (a, b){
-    return(a + b);
-   }
-   console.log(add(2, 3));
+//let add = function (a, b){
+  //  return(a + b);
+  // }
+   //console.log(add(2, 3));
 
 var name = "sonam"
 console.log(name)
