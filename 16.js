7118e2899b2b7da19d139dc35d0f7df8d9ebf27e
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-11-15 14:53:03 +0530

    16js running

--- 16.js
+++ 16.js
@@ -1,13 +1,20 @@
 var name ="sonam"
+console.log(name)
+
 function level1(){
     var name = "ram"
     console.log(name)
 }
 level1()
 
-function level2(){
-    var name = "shyam"
-    console.log(name)
-}
+//function level2(){
+    //var name = "shyam"
+    //console.log(name)
+//}
+//level2()
 
-
+//function level3(){
+    //var name = ""
+    //console.log()
+//}
+//level3()
