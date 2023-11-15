5eeb92c1c90401f2f9912619c563001e5f0b1b52
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-11-15 18:24:17 +0530

    nwArr running

--- 18.js
+++ 18.js
@@ -1,3 +1,9 @@
 const numbers = [4, 9, 16, 25]
 
-const newArr = numbers.map(num => Math.sqrt(num))
+//const newArr = numbers.map(num => Math.sqrt(num))
+//const newArr = numbers.map( Math.sqrt(num))
+const newArr = numbers.map(myFunction)
+function myFunction (num){
+return Math.sqrt(num)
+}
+console.log(newArr)
