9da4bf6bdb8af204b0ddf3f8f30bbf889fc13e6b
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-11-15 13:02:34 +0530

    function level running

--- 16.js
+++ 16.js
@@ -1,15 +1,13 @@
 var name ="sonam"
-
-
 function level1(){
     var name = "ram"
     console.log(name)
 }
-level1{}
+level1()
 
 function level2(){
     var name = "shyam"
-    console.log
+    console.log(name)
 }
 
 
