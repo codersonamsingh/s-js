d186caae3c7a266ea94fb6215d5ab88f0b76b47c
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-11-15 18:31:55 +0530

    nw Arr running

--- 18.js
+++ 18.js
@@ -1,9 +1,52 @@
-const numbers = [4, 9, 16, 25]
+const ages = [32, 33, 16, 40];
+
+let newAr = ages.filter (age => age>18)    
+
+function checkAdult (age){
+    return age >=18;
+}
+console.log(ages)
+console.log(newAr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const numbers = [4, 9, 16, 25]
 
 //const newArr = numbers.map(num => Math.sqrt(num))
 //const newArr = numbers.map( Math.sqrt(num))
-const newArr = numbers.map(myFunction)
-function myFunction (num){
-return Math.sqrt(num)
-}
-console.log(newArr)
+//const newArr = numbers.map(myFunction)
+//function myFunction (num){
+//return Math.sqrt(num)
+//}
+//console.log(newArr)
