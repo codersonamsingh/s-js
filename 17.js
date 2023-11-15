a957a8f41bcd21246a294b997a20ab411d381eb0
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-11-15 17:48:57 +0530

    17.js com p

--- 17.js
+++ 17.js
@@ -1,11 +1,12 @@
-var names = ["Sonam","radha","sita"];
+//var names = ["Sonam","radha","sita"];
 
-console.log(names);
+//console.log(names);
 
 
 
-names.push("krishna")
-console.log(names);
+//names.push("krishna")
+//console.log(names);
 
-//var names = new Array(2);
-//console.log(names)
+var names = new Array(2);
+names = ["ram","sita","sonam"]
+console.log(names)
