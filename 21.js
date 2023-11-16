b001704fbb18dd9897d4b2fe14d8eee63de4ec51
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-11-16 18:27:25 +0530

    221.js running

--- 21.js
+++ 21.js
@@ -2,7 +2,7 @@ let person = {
     firstname:'sonam',
     lastname:"singh",
     age:22,
-    fullName: function(){this.firstname + " " + this.lastname}
+    fullName: function(){return this.firstname + " " + this.lastname}
     //'eye colour' : "black"
 }
-console.log(person.fullName)
+console.log(person.fullName())
