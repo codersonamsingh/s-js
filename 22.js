3aef9e1c282d585c22716005c5cf21e50b4b93b9
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-11-20 17:31:00 +0530

    loop running

--- 22.js
+++ 22.js
@@ -1,4 +1,8 @@
-for ( X=0; X<=5; X++){
+//for ( X=0; X<=5; X++){
     //0 1 2 3 4 5 
-    console.log("l love learn with sonam")
+    //console.log("l love learn with sonam")
+//}
+
+for (let x=1; x<=100; x++){
+    console.log("loop number" + x)
 }
