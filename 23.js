b8f6a257d70044039ca3dfad33c394ff35c42744
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-11-20 18:43:48 +0530

    sum running

--- 23.js
+++ 23.js
@@ -1,6 +1,15 @@
-let x= 0;
-while (x<=5){
-    console.log(x)
+//let x= 0;
+//while (x<=5){
+  //  console.log(x)
 
+    //x++
+//}
+
+let sum = 0
+let x = 1;
+while (sum<3000){
+    sum = sum+x
     x++
 }
+console.log('here x = ${x} and sum is $ {sum}')
+x ++
