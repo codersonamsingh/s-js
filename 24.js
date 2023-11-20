4a6f29d0941dbc2ac6bec168b72edcb4a058e24c
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-11-20 22:02:05 +0530

    24 js

--- 24.js
+++ 24.js
@@ -1,6 +1,11 @@
-let arr = [1,2,3,4,5,6]
-let sum = 0;
-for (x=0;x<arr.length;x++){
-    sum = sum+arr[x]
+//let arr = [1,2,3,4,5,6]
+//let sum = 0;
+//for (x=0;x<arr.length;x++){
+  //  sum = sum+arr[x]
+//}
+//console.log(sum)
+
+const arrya1 = ['a','b','c'];
+for (const element of arrya1){
+    console.log(element);
 }
-console.log(sum)
