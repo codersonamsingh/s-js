6055f43a97558f6e9a7138778a03bfde5226257a
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-11-20 18:11:37 +0530

    consol log running

--- 22.js
+++ 22.js
@@ -3,6 +3,22 @@
     //console.log("l love learn with sonam")
 //}
 
-for (let x=1; x<=100; x++){
-    console.log("loop number" + x)
+//for (let x=1; x<=100; x++){
+   // console.log("loop number" + x)
+//}
+
+//let sum = 0
+//for (let x=1; x<=100; x=x+2){
+  //  sum = sum + x
+//}
+//console.log ("sum is =" + sum)
+
+
+for(let x=0;x>=0;x++){
+    console.log(x)
 }
+
+
+
+
+
