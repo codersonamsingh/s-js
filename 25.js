f8d3884164f668c60041c1433f6f60323e8514ed
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-11-21 17:14:18 +0530

    25.js running

--- 25.js
+++ 25.js
@@ -1,5 +1,8 @@
 (function () {
-    today = new Date
-}
-)
-console.log(Date)
+    today = new Date()
+    date = today.getDate();
+    month = today.getMonth()+1;
+    year = today.getFullYear();
+
+    console.log(Date)
+}) ();
